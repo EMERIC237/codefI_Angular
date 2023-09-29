3df8e217c0d2268b347746ba8480997e902df61a
Emeric FOKOUONG TOUOLAC <64689084+EMERIC237@users.noreply.github.com>
2023-09-29 01:36:31 +0000

    working on article

---
+++ exercise_class_one/src/app/article/article.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-article",
+    templateUrl: './article.component.html'
+})
+export class ArticleComponent {
+    title:string = "Whatever you want";
+    content: string = "Some content goes here. "
+}
